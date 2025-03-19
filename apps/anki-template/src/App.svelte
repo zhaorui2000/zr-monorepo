096d64322cccef197fc882429739140160294906
Zhao Rui <wiserui1996@hotmail.com>
2025-03-19 14:04:43 +0800

    feat: 解决 anki webview 样式 覆盖

--- apps/anki-template/src/App.svelte
+++ apps/anki-template/src/App.svelte
@@ -1,5 +1,14 @@
 <script>
   import MCQs from "./pages/MCQs/Back.svelte";
+  import { onMount } from "svelte";
+  onMount(() => {
+    // Remove Anki default import webview.css
+    document.querySelectorAll('link[rel="stylesheet"]').forEach((link) => {
+      if (link.getAttribute("href").includes("webview.css")) {
+        link.remove();
+      }
+    });
+  });
 </script>
 
 <MCQs />
