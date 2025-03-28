bc3b2ba493089567b3db5b27eec533e6ae98f58c
Zhao Rui <wiserui1996@hotmail.com>
2025-03-28 17:16:31 +0800

    feat: 补充模块支持显示折叠内容 更好的展示 AI 思考过程

--- apps/anki-template/src/modules/Extension.svelte
+++ apps/anki-template/src/modules/Extension.svelte
@@ -2,12 +2,20 @@
   import Card from "@zr/ui/Card";
   import { marked } from "marked";
   import markedRender from "@utils/markedRender";
+  import Collapse from "@zr/ui/Collapse";
   const extension = "{{补充}}"
     .replaceAll(/<br\s*(\/?)>/g, "\n")
     .replaceAll(/&nbsp;/g, " ");
+  const extensionCollapsible = "{{折叠内容}}";
+  const hasExtensionCollapsible = "{{#折叠内容}}1{{/折叠内容}}";
   marked.use({ renderer: markedRender });
 </script>
 
 <Card color="warning" title="补充">
+  {#if Number(hasExtensionCollapsible) === 1}
+    <Collapse color="neutral" icon="plus" title="折叠内容">
+      {@html marked(extensionCollapsible)}
+    </Collapse>
+  {/if}
   {@html marked(extension)}
 </Card>
---
+++ packages/ui/src/components/Collapse.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { cva, cx } from "class-variance-authority";
+  let { color, border, icon, title, className, children } = $props();
+  const collapseVariants = cva("collapse", {
+    variants: {
+      color: {
+        undefined: ["bg-base-100"],
+        none: [""],
+        neutral: ["bg-neutral text-neutral-content"],
+        primary: ["bg-primary text-primary-content"],
+        secondary: ["bg-secondary text-secondary-content"],
+        accent: ["bg-accent text-accent-content"],
+        info: ["bg-info text-info-content"],
+        success: ["bg-success text-success-content"],
+        warning: ["bg-warning text-warning-content"],
+        error: ["bg-error text-error-content"],
+      },
+      border: {
+        true: ["border", "border-base-300"],
+      },
+      icon: {
+        undefined: [""],
+        arrow: ["collapse-arrow"],
+        plus: ["collapse-plus"],
+      },
+    },
+    compoundVariants: [],
+    defaultVariants: {},
+  });
+</script>
+
+<div
+  tabindex="0"
+  class={cx(collapseVariants({ color, border, icon }), className)}
+>
+  <div class="collapse-title font-semibold">{title}</div>
+  <div class="collapse-content text-sm">
+    {@render children?.()}
+  </div>
+</div>
