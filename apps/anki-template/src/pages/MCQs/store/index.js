8f9bcb831f691c484e9724dd5e296be8c28fa5e4
Zhao Rui <wiserui1996@hotmail.com>
2025-04-01 10:51:37 +0800

    feat: 单选题选项不区分大小写

--- apps/anki-template/src/pages/MCQs/store/index.js
+++ apps/anki-template/src/pages/MCQs/store/index.js
@@ -1,7 +1,7 @@
 import { persistentAtom } from "@nanostores/persistent";
 export const OPTION_LABELS = ["A", "B", "C", "D"];
 export const ANKI_QUESTION = "{{问题}}";
-export const ANKI_TRUE_ANSWER = "{{答案}}";
+export const ANKI_TRUE_ANSWER = "{{答案}}".toUpperCase();
 export const ANKI_OPTIONS = ["{{A}}", "{{B}}", "{{C}}", "{{D}}"];
 export const answer = persistentAtom("answer","")
 export const randomOrder = persistentAtom("randomOrder",[1,2,3,4],{
