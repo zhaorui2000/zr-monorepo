e1da8cc5b5b6c1210df29e908283a00dbc96143a
Zhao Rui <wiserui1996@hotmail.com>
2025-04-30 00:52:26 +0800

    feat: test

--- apps/board-game-tools/vite.config.js
+++ apps/board-game-tools/vite.config.js
@@ -1,20 +1,22 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit(), tailwindcss()],
-  build: {
-    assetsInlineLimit: 0, // 禁用资源内联
-  },
+  plugins: [wasm(), topLevelAwait(), sveltekit(), tailwindcss()],
+  // build: {
+  //   assetsInlineLimit: 0, // 禁用资源内联
+  // },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
     optimizeDeps: {
-      exclude: ["@sqlite.org/sqlite-wasm"],
+      exclude: [],
     },
   },
 }));
