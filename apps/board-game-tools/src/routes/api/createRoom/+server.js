e06eb3ee8dd4bdace0e24914c8256084d57c063f
Zhao Rui <wiserui1996@hotmail.com>
2025-04-29 23:44:51 +0800

    feat: test

--- apps/board-game-tools/src/routes/api/createRoom/+server.js
+++ apps/board-game-tools/src/routes/api/createRoom/+server.js
@@ -3,22 +3,25 @@ import DbHandler from "$lib/server/DbHandler.js";
 import getHostAddress from "$lib/server/getHostAddress.js";
 
 export async function GET() {
+  let result = {};
+
   try {
     const dbHandler = new DbHandler("board_game_tools.sqlite");
     const db = await dbHandler.init();
     const ip = getHostAddress();
-    console.log(ip);
     try {
       db.run("INSERT INTO rooms (ip) VALUES (:ip)", {
         ":ip": ip,
       });
     } catch (error) {
       console.error(error);
+      result = { error };
     } finally {
       await dbHandler.endSQL();
     }
   } catch (error) {
     console.error(error);
+    result = { error };
   }
-  return json({});
+  return json(result);
 }
