d2b6ae47fe8d355af8f3561546e1804f276308cd
rui <wiserui1996@hotmail.com>
2025-09-26 22:00:31 +0800

    feat: 空之轨迹 the 1st 魔法组合弹框额外展示需要的 空槽 信息

--- apps/board-game-tools/src/routes/sora_no_kiseki_the_1st/modules/MagicTable/CrystalCircuitCombo.svelte
+++ apps/board-game-tools/src/routes/sora_no_kiseki_the_1st/modules/MagicTable/CrystalCircuitCombo.svelte
@@ -8,9 +8,12 @@
 <Model className="h-[60dvh]" {isShow} {onchange}>
   {#snippet title()}{magicName}{/snippet}
   <div class="space-y-2 p-2">
-    {#each validCombosForMagic.get(magicName) as { comboNames }, index}
+    {#each validCombosForMagic.get(magicName) as { comboNames, size }, index}
       <Alert className="flex gap-x-4" closable>
-        <Badge color="primary">{index + 1}</Badge>
+        <div class="flex items-center gap-2">
+          <Badge color="primary">{index + 1}</Badge>
+          <Badge color="info">{size}槽</Badge>
+        </div>
         <div class="flex gap-1 flex-wrap">
           {#each comboNames as name}
             <Badge>{name}</Badge>
