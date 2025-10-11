ad4ffb228d2afd56bbf9172e8e7398324ab79b4d
rui <wiserui1996@hotmail.com>
2025-10-12 00:26:54 +0800

    feat: ui Table

---
+++ packages/ui/src/components/Table.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { cva, cx } from "class-variance-authority";
+  let { thead, tbody, size, zebra, pinRows, pinCols } = $props();
+  const tableVariants = cva("table", {
+    variants: {
+      size: {
+        xs: "table-xs",
+        sm: "table-sm",
+        md: "table-md",
+        lg: "table-lg",
+        xl: "table-xl",
+      },
+      zebra: {
+        true: "table-zebra",
+      },
+      pinRows: {
+        true: "table-pin-rows",
+      },
+      pinCols: {
+        true: "table-pin-cols",
+      },
+    },
+  });
+</script>
+
+<table class={cx(tableVariants({size,zebra,pinRows,pinCols}))}>
+  <!-- head -->
+  <thead>
+    {@render thead?.()}
+  </thead>
+  <tbody>
+    {@render tbody?.()}
+  </tbody>
+</table>
