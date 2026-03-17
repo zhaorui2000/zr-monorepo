0f6f625ded3c78f39fef34734601fb30ea3b3956
zhaorui <wiserui1996@hotmail.com>
2026-03-17 12:30:29 +0800

    fix(markedHooks): 修复中文引号加粗的markdown渲染问题

--- apps/anki-template/src/utils/markedHooks.svelte.js
+++ apps/anki-template/src/utils/markedHooks.svelte.js
@@ -3,6 +3,7 @@ export default {
     return markdown
       .replace(/\n/g, "\n\n")
       .replace(/\n{3,}/g, "\n\n")
+      .replace(/\*\*“([\u4e00-\u9fa5]+)”\*\*/g, '"**$1**"')
       .replace(/\$\$(.*?)\$\$|\$(.*?)\$/gs, (_, display, inline) => {
         try {
           return katex.renderToString(inline || display, { throwOnError: false });
